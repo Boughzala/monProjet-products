05839c74f72a4b4d7f1b1b2fe9edba5674ed4dcf
unknown <Aba@sagemcom.com>
2018-04-03 10:27:36 +0100

    add new product

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -15,7 +15,8 @@ export class ProductListComponent implements OnInit {
       {code: 'p1', label : 'Cafe', unitPrice: 500.5},
       {code: 'p2', label : 'Tea', unitPrice: 800.5},
       {code: 'p3', label : 'Jus', unitPrice: 400.5},
-      {code: 'p3', label : 'Coca-cola', unitPrice: 600.5}
+      {code: 'p4', label : 'Coca-cola', unitPrice: 600.5},
+      {code: 'p5', label : 'Water', unitPrice: 600.5}
   ];
 
    }
