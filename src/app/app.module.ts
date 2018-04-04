96489f0ca37932daf26357b691fe9200ba4b5c6a
DESKTOP-COPSUH4\Lab03 <Aba@sagemcom.com>
2018-04-04 14:17:59 +0100

    refactoring router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule , Route} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -8,19 +8,30 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { FooterComponent } from './components/footer/footer.component';
 import { ProductService } from './services/product.service';
 import { HttpClientModule } from '@angular/common/http';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
+const rootes: Route[] = [
+  {path : 'welcome', component : WelcomeComponent},
+  {path : 'list', component : ProductListComponent},
+  {path : '', component : WelcomeComponent, pathMatch: 'full'},
+  {path : '**', component : NotFoundComponent},
 
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ProductListComponent,
-    FooterComponent
+    FooterComponent,
+    WelcomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(rootes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
